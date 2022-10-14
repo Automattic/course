233814fc882027e65356def9e12a9de9c0685031
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-14 16:34:29 -0300

    Use the pagination pattern on the search template

---
+++ patterns/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Title: Default Pagination 
+ * Slug: course/pagination
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:group {"layout":{"inherit":false,"type":"constrained"}} -->
+<div class="wp-block-group">
+  <!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+
+  <!-- wp:query-pagination-numbers /-->
+
+  <!-- wp:query-pagination-next {"label":"NEXT"} /-->
+  <!-- /wp:query-pagination -->
+</div>
+<!-- /wp:group -->
