00cd297991483f97acc16a74e1c6fb4f1a5662f3
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 19:18:38 -0300

    Move the sensei messages before move the cover

--- assets/js/cover.js
+++ assets/js/cover.js
@@ -10,21 +10,25 @@ function domReady (callback) {
 
 
 function moveToTop() {
+  console.log('moveToTop')
   const shouldMoveTheCover = document.querySelector(".move-cover");
   if (!shouldMoveTheCover) return false;
-  
+
   const elementsToStick = document.querySelectorAll(".stick-on-top");
   const senseiMessages = Array.from(document.querySelectorAll(".sensei-message"));
   const covers = Array.from(elementsToStick);
   const lastCover = covers[covers.length - 1];
 
+  console.log({ covers });
+  senseiMessages.map(element => {
+    lastCover.parentElement.insertBefore(element, lastCover.nextSibling)
+  });
+  
   covers.map(element => {
+    console.log(element.offsetTop);
     element.style.marginTop = `-${element.offsetTop}px`
   });
 
-  senseiMessages.map(element => {
-    last.parentElement.insertBefore(element, lastCover.nextSibling)
-  });
 }
 
 domReady(moveToTop)
