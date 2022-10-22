23521af88eada0fa7daef1b71b645748788e7cf1
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 17:34:51 -0300

    Add learn pattern

---
+++ patterns/learn.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Title: Cover for courses
+ * Slug: course/learn
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
+<div class="wp-block-group has-white-color has-primary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
+  <!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}},"textColor":"white"} -->
+  <h2 class="has-white-color has-text-color" style="margin-bottom:var(--wp--preset--spacing--60)">What you will learn</h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">How to write</h4>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50","top":"0"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-top:0;padding-bottom:var(--wp--preset--spacing--50)">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">The right pen</h4>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Paper or digital</h4>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|40"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">The blank paper</h4>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Curabitur pellentesque lorem vel fermentum suscipit. Vivamus pretium arcu lectus, in volutpat mi condimentum.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:sensei-lms/button-take-course {"className":"is-style-secondary"} -->
+  <div class="wp-block-sensei-lms-button-take-course is-style-secondary wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link">Take Course</button></div>
+  <!-- /wp:sensei-lms/button-take-course -->
+</div>
+<!-- /wp:group -->
