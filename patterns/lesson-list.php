ce8f89c13186db3a4025275248cda4715cc5c81b
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 19:18:59 -0300

    Add the lesson list pattern

---
+++ patterns/lesson-list.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Title: List of courses
+ * Slug: course/lesson-list
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|30","bottom":"var:preset|spacing|80","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
+<div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--30)">
+  <!-- wp:heading {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"var:preset|spacing|40"}}},"textColor":"background"} -->
+  <h2 class="has-background-color has-text-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--40)">Course Lessons</h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:sensei-lms/course-outline -->
+  <!-- wp:sensei-lms/course-outline-lesson {"id":4164,"title":"Lesson 1"} /-->
+
+  <!-- wp:sensei-lms/course-outline-lesson {"id":4166,"title":"Lesson 2"} /-->
+
+  <!-- wp:sensei-lms/course-outline-lesson {"id":4168,"title":"Lesson 3"} /-->
+  <!-- /wp:sensei-lms/course-outline -->
+
+  <!-- wp:sensei-lms/button-take-course {"className":"is-style-secondary"} -->
+  <div class="wp-block-sensei-lms-button-take-course is-style-secondary wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link">Take Course</button></div>
+  <!-- /wp:sensei-lms/button-take-course -->
+</div>
+<!-- /wp:group -->
