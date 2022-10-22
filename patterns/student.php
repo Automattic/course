c5daef7957bf724f2cc1330575794033845b32c0
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 19:19:26 -0300

    Add the student patttern

---
+++ patterns/student.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Title: Cover for courses
+ * Slug: course/student
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"blockGap":"0.5rem","margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained","contentSize":"900px","wideSize":"1200px"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+  <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group">
+    <!-- wp:image {"align":"center","id":3172,"width":120,"height":200,"sizeSlug":"thumbnail","linkDestination":"none","style":{"border":{"radius":"120px"}},"className":"is-style-rounded"} -->
+    <figure class="wp-block-image aligncenter size-thumbnail is-resized has-custom-border is-style-rounded"><img src="/wp-content/themes/sensei-theme/assets/images/student-placeholder.jpg" alt="" class="wp-image-3172" style="border-radius:120px" width="120" height="200" /></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:pullquote {"style":{"typography":{"lineHeight":1}},"className":"is-style-pullquote-no-spacing is-style-no-spacing","fontSize":"x-large"} -->
+    <figure class="wp-block-pullquote is-style-pullquote-no-spacing is-style-no-spacing has-x-large-font-size" style="line-height:1">
+      <blockquote>
+        <p>“I always wanted to write, and thanks to Cours, I got it right. My writing is clearer, and I can finally get my message across.”</p><cite><strong>Maya Green</strong> - Student</cite>
+      </blockquote>
+    </figure>
+    <!-- /wp:pullquote -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
