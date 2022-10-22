6fcb3d0b1a78f050fcffae5dc47f8600a989c434
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 19:19:43 -0300

    Add the teacher pattern

---
+++ patterns/teacher.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Title: An teacher profile
+ * Slug: course/teacher
+ * Inserter: yes
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained","contentSize":"830px","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--70);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+  <!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+  <div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+    <!-- wp:column {"style":{"border":{"width":"0px","style":"none"},"spacing":{"blockGap":"0"}}} -->
+    <div class="wp-block-column" style="border-style:none;border-width:0px">
+      <!-- wp:image {"id":4005,"sizeSlug":"large","linkDestination":"none","style":{}} -->
+      <figure class="wp-block-image size-large"><img src="/wp-content/themes/sensei-theme/assets/images/placeholder-1.jpg" alt="" class="wp-image-4005" /></figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+    <div class="wp-block-column is-vertically-aligned-center">
+      <!-- wp:group {"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"style":{"typography":{"textTransform":"none","fontStyle":"normal","fontWeight":"500"}},"fontFamily":"system"} -->
+        <h2 class="has-system-font-family" style="font-style:normal;font-weight:500;text-transform:none">Meet Jeff</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>Curabitur pellentesque lorem vel fermentum suscipit. Vivamus pretium arcu lectus, in volutpat mi condimentum.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:sensei-lms/button-contact-teacher {"className":"is-style-default"} -->
+        <div class="wp-block-sensei-lms-button-contact-teacher is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><a class="wp-block-button__link">Contact Teacher</a></div>
+        <!-- /wp:sensei-lms/button-contact-teacher -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:column -->
+  </div>
+  <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
