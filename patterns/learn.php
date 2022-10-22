45341e801aee0cd32307266708470db2ef31396e
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 19:18:13 -0300

    Adjust the learn pattern

--- patterns/learn.php
+++ patterns/learn.php
@@ -5,16 +5,16 @@
  * Slug: course/learn
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
-<div class="wp-block-group has-white-color has-primary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
-  <!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}},"textColor":"white"} -->
-  <h2 class="has-white-color has-text-color" style="margin-bottom:var(--wp--preset--spacing--60)">What you will learn</h2>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|40","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|50"}},"backgroundColor":"primary","textColor":"white","className":"has-system-font-family","layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
+<div class="wp-block-group has-system-font-family has-white-color has-primary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
+  <!-- wp:heading {"textColor":"white"} -->
+  <h2 class="has-white-color has-text-color">What you will learn</h2>
   <!-- /wp:heading -->
 
-  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"default"}} -->
   <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">How to write</h4>
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|40","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">How to Write</h4>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph {"fontSize":"x-small"} -->
@@ -25,8 +25,8 @@
 
   <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50","top":"0"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
   <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-top:0;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">The right pen</h4>
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|40","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">The right pen</h4>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph {"fontSize":"x-small"} -->
@@ -37,8 +37,8 @@
 
   <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
   <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Paper or digital</h4>
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--50);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">Paper or digital</h4>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph {"fontSize":"x-small"} -->
@@ -49,8 +49,8 @@
 
   <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|40"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
   <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-    <h4 class="has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">The blank paper</h4>
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--50);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">The blank paper</h4>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph {"fontSize":"x-small"} -->
