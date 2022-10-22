4419fc5eb77fc498f226020a626a43ebfbb941e0
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 17:13:35 -0300

    Add template to modify the menu

---
+++ patterns/page-course-with-cover.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: Course Landing Page
+ * Slug: course/landing-page
+ * Categories: sensei-lms
+ * Block Types: sensei-lms/post-content
+ * Inserter: yes
+ */
+?>
+<!-- wp:pattern {"slug":"course/cover"} /-->
+<!-- wp:pattern {"slug":"course/author"} /-->
+<!-- wp:pattern {"slug":"course/learn"} /-->
+<!-- wp:pattern {"slug":"course/teacher"} /-->
+<!-- wp:pattern {"slug":"course/work"} /-->
+<!-- wp:pattern {"slug":"course/student"} /-->
+<!-- wp:pattern {"slug":"course/lesson-list"} /-->
