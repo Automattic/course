8d0683404516f979054020e98fc6589187777596
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-22 19:17:21 -0300

    Adjust the author template

--- patterns/author.php
+++ patterns/author.php
@@ -6,21 +6,21 @@
  * Inserter: yes
  */
 ?>
-<!-- wp:group -->
-<div class="wp-block-group">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
   <!-- wp:heading -->
   <h2>Heading Two</h2>
   <!-- /wp:heading -->
 
-  <!-- wp:paragraph -->
-  <p>La serie prevede 6 episodi, con uscita settimanale dall’8 gennaio 2021. Gli episodi saranno disponibili all’ascolto
+  <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+  <p style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">La serie prevede 6 episodi, con uscita settimanale dall’8 gennaio 2021. Gli episodi saranno disponibili all’ascolto
     su Spotify, Spreaker, Apple Podcast e su tutte le principali app gratuite per l’ascolto dei Podcast.</p>
   <!-- /wp:paragraph -->
 
   <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"},"margin":{"top":"var:preset|spacing|60"}}}} -->
   <div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--60)">
-    <!-- wp:column -->
-    <div class="wp-block-column">
+    <!-- wp:column {"width":"50%","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-column" style="flex-basis:50%">
       <!-- wp:image {"id":1309,"sizeSlug":"full","linkDestination":"none"} -->
       <figure class="wp-block-image size-full"><img src="/wp-content/themes/sensei-theme/assets/images/placeholder-2.jpg" alt="" class="wp-image-1309" /></figure>
       <!-- /wp:image -->
@@ -30,36 +30,28 @@
     <!-- wp:column -->
     <div class="wp-block-column"></div>
     <!-- /wp:column -->
-
-    <!-- wp:column {"width":"10%"} -->
-    <div class="wp-block-column" style="flex-basis:10%"></div>
-    <!-- /wp:column -->
   </div>
   <!-- /wp:columns -->
 
-  <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
-  <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-    <!-- wp:columns -->
-    <div class="wp-block-columns">
-      <!-- wp:column {"width":"45%"} -->
-      <div class="wp-block-column" style="flex-basis:45%"></div>
-      <!-- /wp:column -->
+  <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+  <div class="wp-block-columns">
+    <!-- wp:column {"width":"%"} -->
+    <div class="wp-block-column"></div>
+    <!-- /wp:column -->
 
-      <!-- wp:column {"width":"55%"} -->
-      <div class="wp-block-column" style="flex-basis:55%">
-        <!-- wp:quote {"align":"left","className":"is-style-quote-no-spacing is-style-default"} -->
-        <blockquote class="wp-block-quote has-text-align-left is-style-quote-no-spacing is-style-default">
-          <!-- wp:paragraph {"align":"left"} -->
-          <p class="has-text-align-left">Blog managed by Prof Jean M. Bichè, Chair of Professor of Creative Writing
-            at Université Sacré-Cœur Paris.</p>
-          <!-- /wp:paragraph --><cite>The Author</cite>
-        </blockquote>
-        <!-- /wp:quote -->
-      </div>
-      <!-- /wp:column -->
+    <!-- wp:column {"width":"56%"} -->
+    <div class="wp-block-column" style="flex-basis:56%">
+      <!-- wp:quote {"align":"left","className":"is-style-quote-no-spacing is-style-default"} -->
+      <blockquote class="wp-block-quote has-text-align-left is-style-quote-no-spacing is-style-default">
+        <!-- wp:paragraph {"align":"left"} -->
+        <p class="has-text-align-left">Blog managed by Prof Jean M. Bichè, Chair of Professor of Creative Writing
+          at Université Sacré-Cœur Paris.</p>
+        <!-- /wp:paragraph --><cite>The Author</cite>
+      </blockquote>
+      <!-- /wp:quote -->
     </div>
-    <!-- /wp:columns -->
+    <!-- /wp:column -->
   </div>
-  <!-- /wp:group -->
+  <!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
