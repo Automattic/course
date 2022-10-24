73331765814cfc083618f8a463f16acd60d6cde1
Imran Hossain <imranh920@gmail.com>
2022-10-25 03:46:45 +0600

    Update functions.php
    Remove pattern category registration

--- functions.php
+++ functions.php
@@ -68,33 +68,6 @@ function course_theme_init() {
 			'label' => __( 'Button', 'course' ),
 		)
 	);
-
-	$block_pattern_categories = array(
-		'sensei-lms' => array( 'label' => __( 'Sensei LMS', 'course' ) )
-	);
-
-	/**
-	 * Filters the theme block pattern categories.
-	 *
-	 * @since Course 1.0
-	 *
-	 * @param array[] $block_pattern_categories {
-	 *     An associative array of block pattern categories, keyed by category name.
-	 *
-	 *     @type array[] $properties {
-	 *         An array of block category properties.
-	 *
-	 *         @type string $label A human-readable label for the pattern category.
-	 *     }
-	 * }
-	 */
-	$block_pattern_categories = apply_filters( 'course_block_pattern_categories', $block_pattern_categories );
-
-	foreach ( $block_pattern_categories as $name => $properties ) {
-		if ( ! WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $name ) ) {
-			register_block_pattern_category( $name, $properties );
-		}
-	}
 }
 
 add_action( 'init', 'course_theme_init' );
