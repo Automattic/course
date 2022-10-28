e1b9dbde67430808536f24733df905cb13e5ef57
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-28 13:17:37 -0300

    Fix learn pattern

--- patterns/learn.php
+++ patterns/learn.php
@@ -5,56 +5,56 @@
  * Slug: course/learn
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|40","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|50","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"white","className":"has-system-font-family","layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
-<div class="wp-block-group has-system-font-family has-white-color has-primary-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
-  <!-- wp:heading {"textColor":"white"} -->
-  <h2 class="has-white-color has-text-color">What you will learn</h2>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|40","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40"},"blockGap":"1.25rem","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"background","className":"has-system-font-family","layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
+<div class="wp-block-group has-system-font-family has-background-color has-primary-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
+  <!-- wp:heading {"style":{"typography":{"lineHeight":"1","letterSpacing":"-1px","fontStyle":"normal","fontWeight":"600"}},"textColor":"background","fontFamily":"system"} -->
+  <h2 class="has-background-color has-text-color has-system-font-family" style="font-style:normal;font-weight:600;letter-spacing:-1px;line-height:1">What you will learn</h2>
   <!-- /wp:heading -->
 
-  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"default"}} -->
-  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|40","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
-    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">How to Write</h4>
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"1.25rem"},"blockGap":"1.25rem"}},"textColor":"tertiary","layout":{"type":"default"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:1.25rem">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|40","left":"0"}},"typography":{"letterSpacing":"-1px","fontStyle":"normal","fontWeight":"700"}},"textColor":"background","fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-background-color has-text-color has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0;font-style:normal;font-weight:700;letter-spacing:-1px">How to Write</h4>
     <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"fontSize":"x-small"} -->
-    <p class="has-x-small-font-size">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
+    <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3"}},"textColor":"background","fontFamily":"eb-garamond"} -->
+    <p class="has-background-color has-text-color has-eb-garamond-font-family" style="line-height:1.3">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
 
-  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50","top":"0"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
-  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-top:0;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|40","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
-    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">The right pen</h4>
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"1.25rem","top":"0"},"blockGap":"1.25rem"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-top:0;padding-bottom:1.25rem">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|40","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"textColor":"background","fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-background-color has-text-color has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">The right pen</h4>
     <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"fontSize":"x-small"} -->
-    <p class="has-x-small-font-size">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
+    <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3"}},"textColor":"background","fontFamily":"eb-garamond"} -->
+    <p class="has-background-color has-text-color has-eb-garamond-font-family" style="line-height:1.3">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
 
-  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
-  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
-    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--50);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">Paper or digital</h4>
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"1.25rem"},"blockGap":"1.25rem"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:1.25rem">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"textColor":"background","fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-background-color has-text-color has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--50);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">Paper or digital</h4>
     <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"fontSize":"x-small"} -->
-    <p class="has-x-small-font-size">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
+    <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3"}},"textColor":"background","fontFamily":"eb-garamond"} -->
+    <p class="has-background-color has-text-color has-eb-garamond-font-family" style="line-height:1.3">Sed id risus eleifend, suscipit ante a, malesuada purus. Curabitur sapien ligula, ullamcorper sit amet elit vitae, rutrum tempus.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
 
-  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|40"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
-  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"fontFamily":"system"} -->
-    <h4 class="has-text-align-left has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--50);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">The blank paper</h4>
+  <!-- wp:group {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"bottom":"1.25rem"},"blockGap":"1.25rem"}},"textColor":"tertiary","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group has-tertiary-color has-text-color" style="border-bottom-width:1px;padding-bottom:1.25rem">
+    <!-- wp:heading {"textAlign":"left","level":4,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"textColor":"background","fontFamily":"system"} -->
+    <h4 class="has-text-align-left has-background-color has-text-color has-system-font-family" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--50);margin-left:0;font-style:normal;font-weight:600;letter-spacing:-1px">The blank paper</h4>
     <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"fontSize":"x-small"} -->
-    <p class="has-x-small-font-size">Curabitur pellentesque lorem vel fermentum suscipit. Vivamus pretium arcu lectus, in volutpat mi condimentum.</p>
+    <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3"}},"textColor":"background","fontFamily":"eb-garamond"} -->
+    <p class="has-background-color has-text-color has-eb-garamond-font-family" style="line-height:1.3">Curabitur pellentesque lorem vel fermentum suscipit. Vivamus pretium arcu lectus, in volutpat mi condimentum.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
