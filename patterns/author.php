1adcd4a5946b36db0b32ba03cfdd2a0f2b5e6c58
Gabriel Caires <gabriel.caires@automattic.com>
2022-10-28 12:33:38 -0300

    Update author spacings

--- patterns/author.php
+++ patterns/author.php
@@ -6,8 +6,8 @@
  * Inserter: yes
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|30","bottom":"var:preset|spacing|80","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--30)">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"0","bottom":"var:preset|spacing|80","left":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"660px","wideSize":"1200px"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0">
   <!-- wp:heading {"style":{"typography":{"letterSpacing":"-0.01em"},"spacing":{"margin":{"bottom":"1.25rem"}}},"fontFamily":"system"} -->
   <h2 class="has-system-font-family" style="margin-bottom:1.25rem;letter-spacing:-0.01em"><strong>Heading Two</strong></h2>
   <!-- /wp:heading -->
@@ -33,25 +33,32 @@
   </div>
   <!-- /wp:columns -->
 
-  <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
-  <div class="wp-block-columns">
-    <!-- wp:column {"width":"%"} -->
-    <div class="wp-block-column"></div>
-    <!-- /wp:column -->
+  <!-- wp:group {"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group">
+    <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}}} -->
+    <div class="wp-block-columns" style="margin-top:0;margin-bottom:0">
+      <!-- wp:column {"width":"300px","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0"}}} -->
+      <div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:300px"></div>
+      <!-- /wp:column -->
 
-    <!-- wp:column {"width":"56%"} -->
-    <div class="wp-block-column" style="flex-basis:56%">
-      <!-- wp:quote {"align":"left","className":"is-style-quote-no-spacing is-style-default"} -->
-      <blockquote class="wp-block-quote has-text-align-left is-style-quote-no-spacing is-style-default">
-        <!-- wp:paragraph {"align":"left"} -->
-        <p class="has-text-align-left">Blog managed by Prof Jean M. Bichè, Chair of Professor of Creative Writing
-          at Université Sacré-Cœur Paris.</p>
-        <!-- /wp:paragraph --><cite>The Author</cite>
-      </blockquote>
-      <!-- /wp:quote -->
+      <!-- wp:column {"width":"306px","style":{"spacing":{"padding":{"left":"5rem"}}}} -->
+      <div class="wp-block-column" style="padding-left:5rem;flex-basis:306px">
+        <!-- wp:group {"style":{"border":{"left":{"width":"1px"}},"spacing":{"padding":{"top":"0","bottom":"0","left":"1.25rem"},"margin":{"top":"1.25rem","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top","justifyContent":"right"}} -->
+        <div class="wp-block-group" style="border-left-width:1px;margin-top:1.25rem;margin-bottom:0;padding-top:0;padding-bottom:0;padding-left:1.25rem">
+          <!-- wp:quote {"align":"left","style":{"typography":{"letterSpacing":"-1%"}},"className":"is-style-plain"} -->
+          <blockquote class="wp-block-quote has-text-align-left is-style-plain" style="letter-spacing:-1%">
+            <!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"-1%"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"fontSize":"small"} -->
+            <p class="has-text-align-left has-small-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;letter-spacing:-1%">Blog managed by Prof Jean M. Bichè, Chair of Professor of Creative Writing at Université Sacré-Cœur Paris.</p>
+            <!-- /wp:paragraph --><cite>The Author</cite>
+          </blockquote>
+          <!-- /wp:quote -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:column -->
     </div>
-    <!-- /wp:column -->
+    <!-- /wp:columns -->
   </div>
-  <!-- /wp:columns -->
+  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
