a17d82cc754faf69cc862e4d099348deae5f0c77
Donna Peplinskie <donnapep@gmail.com>
2022-11-03 17:47:54 -0600

    Set letter case in block settings so it can be customized

--- patterns/newsletter.php
+++ patterns/newsletter.php
@@ -5,18 +5,17 @@
  * Inserter: no
  */
 ?>
+
 <!-- wp:group {"style":{"spacing":{"blockGap":"1.25rem"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group">
-<!-- wp:heading {"fontSize":"medium"} -->
-<h2 class="has-medium-font-size"><?php echo esc_html__('NEWSLETTER', 'course');?></h2>
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"medium"} -->
+<h2 class="has-medium-font-size" style="text-transform:uppercase"><?php echo esc_html__( 'Newsletter', 'course' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html__('Our latest updates in your e-mail.', 'course'); ?></p>
+<p><?php echo esc_html__( 'Our latest updates in your e-mail.', 'course' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="#"><?php echo esc_html__('Sign up', 'course'); ?></a></p>
-<!-- /wp:paragraph -->
-</div>
+<p><a href="#"><?php echo esc_html__( 'Sign up', 'course' ); ?></a></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
