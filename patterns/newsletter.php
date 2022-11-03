f4addbd6732b9a604529c78a8c3c1bdddf20f929
Donna Peplinskie <donnapep@gmail.com>
2022-11-03 15:24:00 -0600

    Remove some patterns from the inserter

--- patterns/newsletter.php
+++ patterns/newsletter.php
@@ -1,22 +1,22 @@
-    <?php
-    /**
-     * Title: Newsletter text 
-     * Slug: course/newsletter
-     * Inserter: yes
-     */
-    ?>
-    <!-- wp:group {"style":{"spacing":{"blockGap":"1.25rem"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
-    <div class="wp-block-group">
-      <!-- wp:heading {"fontSize":"medium"} -->
-      <h2 class="has-medium-font-size"><?php echo esc_html__('NEWSLETTER', 'course');?></h2>
-      <!-- /wp:heading -->
+<?php
+/**
+ * Title: Newsletter
+ * Slug: course/newsletter
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"1.25rem"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group">
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="has-medium-font-size"><?php echo esc_html__('NEWSLETTER', 'course');?></h2>
+<!-- /wp:heading -->
 
-      <!-- wp:paragraph -->
-      <p><?php echo esc_html__('Our latest updates in your e-mail.', 'course'); ?></p>
-      <!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><?php echo esc_html__('Our latest updates in your e-mail.', 'course'); ?></p>
+<!-- /wp:paragraph -->
 
-      <!-- wp:paragraph -->
-      <p><a href="#"><?php echo esc_html__('Sign up', 'course'); ?></a></p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+<!-- wp:paragraph -->
+<p><a href="#"><?php echo esc_html__('Sign up', 'course'); ?></a></p>
+<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
