df28d61183deeb6c3f66311aec4b66a6438c3f02
Gabriel Caires <gabriel.caires@automattic.com>
2022-11-06 09:28:02 -0300

    Add learning mode CSS customizations

--- functions.php
+++ functions.php
@@ -20,8 +20,11 @@ if ( ! function_exists( 'course_support' ) ) :
 	 */
 	function course_support() {
 
+		add_theme_support('sensei-learning-mode');
+		
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
+		add_editor_style( 'learning-mode.css' );
 
 	}
 
@@ -47,9 +50,21 @@ if ( ! function_exists( 'course_scripts' ) ) :
 			array(),
 			wp_get_theme()->get( 'Version' )
 		);
+		
+		wp_register_style(
+			'course-learning-mode',
+			get_stylesheet_directory_uri() . '/learning-mode.css',
+			array(),
+			wp_get_theme()->get(
+				'Version'
+			)
+		);
 
 		// Enqueue theme stylesheet.
 		wp_enqueue_style( 'course-style' );
+
+		// TODO: Only Load it if the the page is using learning mode
+		wp_enqueue_style('sensei-learning-mode');
 
 		// Enqueque theme scripts.
 		wp_enqueue_script( 'course-header', get_template_directory_uri() . '/assets/js/header.js', [], wp_get_theme()->get( 'Version' ), true );
