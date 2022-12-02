3d56a3004cb18d1a4554a86cddabdec8aaf7306a
Gabriel Caires <gabriel.caires@automattic.com>
2022-12-02 18:20:49 -0300

    Use animation + observer to improve the font-size calculation

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -9,10 +9,8 @@ function domReady ( callback ) {
 	window.addEventListener( 'load', handler, false );
 }
 
-let footerSiteTitle = null;
-let footerSiteTitleText = null;
 
-function calculateTextSize() {
+function calculateTextSize(footerSiteTitle, footerSiteTitleText) {
 	const parentContainerWidth = footerSiteTitleText.parentNode.clientWidth;
 	const currentTextWidth = footerSiteTitleText.scrollWidth;
 	const currentFontSize = parseInt( window.getComputedStyle( footerSiteTitleText ).fontSize );
@@ -20,20 +18,30 @@ function calculateTextSize() {
 	const newValue = Math.min( Math.max( 16, ( parentContainerWidth / currentTextWidth ) * currentFontSize ), 500 );
 
 	footerSiteTitleText.style.setProperty( 'font-size', newValue + 'px' );
-	footerSiteTitle.style.height = footerSiteTitleText.offsetHeight * 0.65 + 'px';
+	footerSiteTitle.style.height = footerSiteTitleText.offsetHeight * 0.65 + 'px';  
 }
 
 domReady( () => {
-	footerSiteTitle = document.querySelector( 'footer .wp-block-site-title' );
-	footerSiteTitleText = document.querySelector( 'footer .wp-block-site-title a' );
+	const footerSiteTitle = document.querySelector( 'footer .wp-block-site-title' );
+	const footerSiteTitleText = document.querySelector( 'footer .wp-block-site-title a' );
 
-	if ( ! footerSiteTitle || ! footerSiteTitleText ) {
+	if ( !footerSiteTitle || !footerSiteTitleText) {
 		return;
 	}
 
-	calculateTextSize();
 
+	calculateTextSize(footerSiteTitle, footerSiteTitleText);
+
+	const inViewport = (entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				footerSiteTitle.classList.add('is-visible');
+			}
+		});
+	};
+
+	new IntersectionObserver(inViewport).observe(footerSiteTitle)
 	addEventListener( 'resize', () => {
-		calculateTextSize();
-	} );
-} );
+		calculateTextSize(footerSiteTitle, footerSiteTitleText);
+	});
+});
